61bc6385eabb167e4d7ac4a8f1a5940af49e28d4
Ryan Sousa <Sousa.Ryan4@gmail.com>
2019-12-11 22:18:46 -0800

    added prompts

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 var generateEl = document.querySelector('#submit');
 var resultEl = document.querySelector('#result');
 var copyEl = document.querySelector('#copy');
+
+var desiredLength = prompt('Choose length of password between 8 and 128 characters');
+
+if (desiredLength >= 129) {
+  alert("You must choose a password lenght between 8 and 128 characters");
+  prompt('Choose length of password between 8 and 128 characters');
+}
+else if (desiredLength <= 7) {
+  alert("You must choose a password lenght between 8 and 128 characters");
+  prompt('Choose length of password between 8 and 128 characters');
+}
+
+var lowercaseCon = confirm('Would you like to use lowercase letters?');
+var uppercaseCon = confirm('Would you like to use uppercase letters?');
+var numberCon = confirm('Would you like to use numbers?');
+var characterCon = confirm('Would you like to use special characters?');
