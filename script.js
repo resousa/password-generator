f77f4bf9e5019907437c709e6d2cd9ff501fe695
Ryan Sousa <Sousa.Ryan4@gmail.com>
2019-12-11 22:40:56 -0800

    prompt value into integer

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var resultEl = document.querySelector('#result');
 var copyEl = document.querySelector('#copy');
 
 var desiredLength = prompt('Choose length of password between 8 and 128 characters');
-
 if (desiredLength >= 129) {
   alert("You must choose a password lenght between 8 and 128 characters");
   prompt('Choose length of password between 8 and 128 characters');
@@ -17,3 +16,7 @@ var lowercaseCon = confirm('Would you like to use lowercase letters?');
 var uppercaseCon = confirm('Would you like to use uppercase letters?');
 var numberCon = confirm('Would you like to use numbers?');
 var characterCon = confirm('Would you like to use special characters?');
+
+var pwLength = parseInt(desiredLength)
+console.log(pwLength)
+
